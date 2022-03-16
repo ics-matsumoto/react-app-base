bfc728dad8bd134bb0552d2efa8f5f0d0acd1cd2
ics-matsumoto <matsumoto@ics-web.jp>
2022-03-16 11:24:06 +0900

    アプリ全体の静的なスタイル定義を追加

--- front/src/components/app/App.tsx
+++ front/src/components/app/App.tsx
@@ -2,6 +2,7 @@ import { VFC } from 'react';
 import { RecoilRoot } from 'recoil';
 import { AppRouter } from './AppRouter';
 import 'modern-css-reset';
+import './app.css';
 
 export const App: VFC = () => {
   return (
